7125e2a37fcb28820ebe8355148591e5440ae4ae
ReZero <ghostofpast@icloud.com>
2022-12-13 20:40:10 +0300

    add determine func

--- test_3_rps(f).py
+++ test_3_rps(f).py
@@ -21,3 +21,21 @@ def get_computer_chice():
     action = Var(choice)
     return action
 
+def determine_winner(user_action, computer_action):
+    if user_action == computer_action:
+        print(f"Оба пользователя выбрали {user_action.name}. Ничья!")
+    elif user_action == Var.Rock:
+        if computer_action == Var.Scissors:
+            print("Камень бьет ножницы! Вы победили!")
+        else:
+            print("Бумага оборачивает камень! Вы проиграли.")
+    elif user_action == Var.Paper:
+        if computer_action == Var.Rock:
+            print("Бумага оборачивает камень! Вы победили!")
+        else:
+            print("Ножницы режут бумагу! Вы проиграли.")
+    elif user_action == Var.Scissors:
+        if computer_action == Var.Paper:
+            print("Ножницы режут бумагу! Вы победили!")
+        else:
+            print("Камень бьет ножницы! Вы проиграли.")
